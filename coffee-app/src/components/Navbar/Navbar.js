c67fd32fc4156e0ec9a0daaab63b6f42a7ea1df4
gjcooper99 <gjcooper99@gmail.com>
2021-08-31 14:14:08 -0700

    Removal of extraneous starter React Files

---
+++ coffee-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <p>Pootis</p>
+    )
+}
+
+export default Navbar
---
+++ coffee-app/src/components/Navbar/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <p>Pootiss</p>
+        </div>
+    )
+}
+
+export default index
