b0f2d7dfa2ddcb836d96fffef02c32bb53ba7a14
gjcooper99 <gjcooper99@gmail.com>
2021-09-16 19:40:50 -0700

    Added banner dividers on menu page

--- coffee-app/src/components/MenuCo/MenuOrganizer.js
+++ coffee-app/src/components/MenuCo/MenuOrganizer.js
@@ -17,6 +17,30 @@ const Menu = () => {
                         )
                     })}
             </div>
+            <Banner heading="Classic Coffees"/>
+            <div className="menulistings">
+            {Specialties.map((item, index) => {
+                        return (
+                            <Card name={item.drink} image={item.image} description={item.description}/>
+                        )
+                    })}
+            </div>
+            <Banner heading="Teas"/>
+            <div className="menulistings">
+            {Specialties.map((item, index) => {
+                        return (
+                            <Card name={item.drink} image={item.image} description={item.description}/>
+                        )
+                    })}
+            </div>
+            <Banner heading="Other Drinks"/>
+            <div className="menulistings">
+            {Specialties.map((item, index) => {
+                        return (
+                            <Card name={item.drink} image={item.image} description={item.description}/>
+                        )
+                    })}
+            </div>
             <video autoPlay loop muted>
                     <source src={bgvideo} type="video/mp4"/>
             </video>
