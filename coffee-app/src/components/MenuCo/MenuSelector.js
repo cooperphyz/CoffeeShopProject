a6bfb9748374c01d2ce5a4dc76dea4566ac95492
gjcooper99 <gjcooper99@gmail.com>
2021-09-20 22:15:30 -0700

    Started on menu selector hamburger menu

--- coffee-app/src/components/MenuCo/MenuSelector.js
+++ coffee-app/src/components/MenuCo/MenuSelector.js
@@ -1,27 +1,53 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './MenuSelector.css'
 import {MenuOptions} from './MenuOptions'
 
 import { Link } from 'react-router-dom';
 
-const Banner = (props) => {
+class MenuSelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            menuopen: false
+        };
+
+        this.menuToggle = this.menuToggle.bind(this);
+    }
+    menuToggle(e) {
+        e.stopPropagation();
+        this.setState({
+            open: !this.state.open
+        });
+        console.log(this.state.open)
+      }
+
+    render () {
     return (
         <div className='BannerItems'>
-            <div className = 'menuheading' style={{backgroundColor: "lightblue"}}>
+            <div className = 'menuheading'>
             {MenuOptions.map((item, index) => {
                         return (
+                            <div className="menu-listings">
                             <li key={index}>
                                 <Link className={item.cName} to={item.url}>
                                     {item.title}
                                 </Link>
                             </li>
+                            </div>
                         )
                     })}
-                <h1>{props.heading}</h1>
+                <h1>{this.props.heading}</h1>
             </div>
-
+            {!this.state.menuopen && <div className ="burgerme" onClick={this.menuToggle}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+            </div>
+                }
         </div>
     )
 }
+}
 
-export default Banner
+
+export default MenuSelector
