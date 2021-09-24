b4a95f7b6c3163630f9730fa239482c0b069dd5b
cooperphyz <gjcooper99@gmail.com>
2021-09-24 08:03:49 -0700

    Started work on Contact page

--- coffee-app/src/Pages/Contact.js
+++ coffee-app/src/Pages/Contact.js
@@ -1,9 +1,17 @@
 import '../App.css';
+import './Contact.css'
+import Banner from '../components/MenuCo/Banner'
+import bgvideo from '../components/video/Keraff.mp4'
 
 function Contact() {
   return (
-    <div className="App">
-      
+    <div className="Contact">
+      <div className="contactcontent">
+        
+      </div>
+      <video class="bgvideo" autoPlay loop muted>
+            <source src={bgvideo} type="video/mp4"/>
+        </video>
     </div>
   );
 }
